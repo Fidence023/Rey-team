c1adea28a1176a71b5890e764e718a46f3251c1a
Godwin oluebube confidence <godwincomfidence18@gmail.com>
2025-12-12 11:33:28 +0100

    i designed all

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "oursaga.html",
+    "one.html",
+    "journal.html",
+    "index.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
